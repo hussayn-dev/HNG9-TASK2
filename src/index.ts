29557aba083fbec4a883293d7c7df1db4476a13c
Hussayn <hussaynabdsamad07@gmail.com>
2022-11-05 13:31:20 +0100

    fixes

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,11 @@ const port = process.env.PORT || 4200;
 app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
